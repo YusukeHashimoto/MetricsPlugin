82a617ff72f9c693d22c367ab8001712f3833af5
Yusuke Hashimoto <yusuke84moto@gmail.com>
2017-10-08 20:42:42 +0900

    テストケースの追加

--- src/codeanalyzer/test/SimpleTest.java
+++ src/codeanalyzer/test/SimpleTest.java
@@ -5,42 +5,53 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-import java.util.List;
+import java.util.*;
 
 import org.junit.BeforeClass;
 import codeanalyzer.ClassInfo;
 import codeanalyzer.CodeAnalyzer;
 
 public class SimpleTest {
-	static CodeAnalyzer analyzer = new CodeAnalyzer();
+	static List<ClassInfo> infoList;
+	private static final Map<String, Integer> WMCMap = new HashMap<>();
+	private static final Map<String, Integer> CBOMap = new HashMap<>();
 
 	@BeforeClass
-	public static void in() {
+	public static void init() {
+		CodeAnalyzer analyzer = new CodeAnalyzer();
 		analyzer.run("src/codeanalyzer/test/");
+		infoList = analyzer.getClassInfo();
+		
+		WMCMap.put("Animal", 5);
+		WMCMap.put("Food", 0);
+		
+		CBOMap.put("Animal", 1);
+		CBOMap.put("Food", 0);
 	}
 	
 	@Test
-	public void testMethodAnalyzer() {
-		List<ClassInfo> infoList = analyzer.getClassInfo();
+	public void testNumberOfClass() {
 		assertEquals(infoList.size(), 3);
+	}
+	
+	@Test
+	public void testWMC() {
 		for(ClassInfo ci : infoList) {
-			assertEquals(ci.getPackageName(), "codeanalyzer.test");
-
-			int wmc = 0;
-			int rfc = 0;
-			switch(ci.getClassName()) {
-			case "Animal":
-				wmc = 5;
-				rfc = 1;
-				break;
-			case "Food":
-				wmc = 0;
-				rfc = 0;
-				break;
-			}
 			if(ci.getClassName().contains("Test")) continue;
-			assertEquals(wmc, ci.weightedMethodsPerClass());
-			assertEquals(rfc, ci.responsesForClass());
+			assertEquals(ci.weightedMethodsPerClass(), (int)WMCMap.get(ci.getClassName()));
+		}
+	}
+	
+	@Test
+	public void testRFC() {
+		
+	}
+	
+	@Test
+	public void testCBO() {
+		for(ClassInfo ci : infoList) {
+			if(ci.getClassName().contains("Test")) continue;
+			assertEquals(ci.efficientCouplings(ClassInfo.COUPLING_LEVEL_CLASS).size(), (int)CBOMap.get(ci.getClassName()));
 		}
 	}
 }
