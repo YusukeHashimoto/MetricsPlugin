6f7a1c561080c9882db9ee8a915345a4f263e26c
Yusuke Hashimoto <yusuke84moto@gmail.com>
2017-11-30 16:15:12 +0900

    MetricsTreeViewのリファクタリングとジャンプ機能の改良

--- src/metricsplugin/views/metricstreeview/MetricsTreeView.java
+++ src/metricsplugin/views/metricstreeview/MetricsTreeView.java
@@ -2,7 +2,6 @@ package metricsplugin.views.metricstreeview;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.eclipse.jface.viewers.*;
 import org.eclipse.swt.widgets.Composite;
@@ -11,16 +10,10 @@ import org.eclipse.ui.part.ViewPart;
 import codeanalyzer.CodeAnalyzer;
 import util.ProjectUtil;
 import warning.Warning;
-import warning.suggestion.SplitMethodSuggestion;
-import warning.suggestion.Suggestion;
 
 public class MetricsTreeView extends ViewPart {
 	private TreeViewer viewer;
 	private List<Warning> warnings = new ArrayList<>();
-
-	public MetricsTreeView() {
-		// TODO Auto-generated constructor stub
-	}
 
 	@Override
 	public Object getAdapter(Class arg0) {
@@ -31,20 +24,17 @@ public class MetricsTreeView extends ViewPart {
 	@Override
 	public void createPartControl(Composite parent) {
 		viewer = new TreeViewer(parent);
-		// viewer.setContentProvider(new ExplororContentProvider());
 		viewer.setContentProvider(new WarningContentProvider());
 		viewer.setLabelProvider(new ExplororLabelProvider());
 		calc();
-		// viewer.setInput(warnings.toArray());
-		// viewer.setInput(MetricsCategory.SIMPLE_METRICS.getWarnings().toArray());
 		viewer.setInput(MetricsCategory.values());
 		viewer.addDoubleClickListener(event -> {
 			ISelection selection = viewer.getSelection();
 			Object obj = ((IStructuredSelection)selection).getFirstElement();
 			if(obj instanceof Warning) {
-				ProjectUtil.openInEditor(((Warning)obj).getFilename());
-				//ProjectUtil.markLine(((Warning)obj).getLine());
-				ProjectUtil.markPosition(((Warning)obj).getNode().getStartPosition());
+				Warning warning = (Warning)obj;
+				ProjectUtil.openInEditor(warning.getFilename());
+				ProjectUtil.markPosition(warning.getNode() != null ? warning.getNode().getStartPosition() : 0);
 			}
 		});
 
@@ -57,21 +47,14 @@ public class MetricsTreeView extends ViewPart {
 
 	}
 
-	private List<String> calc() {
+	private void calc() {
 		try {
 			CodeAnalyzer ca = new CodeAnalyzer();
 			ca.analyzeCodes(null, ProjectUtil.pathToPackage(), ProjectUtil.currentProject());
 			warnings = ca.getWarnings();
 			MetricsCategory.setAllWarnings(warnings);
-
-			List<String> warnings = ca.getWarnings().stream().map(Warning::getMessage).collect(Collectors.toList());
-			if (warnings.isEmpty())
-				warnings.add("問題のあるメトリクスは見つかりませんでした");
-			return warnings;
 		} catch (Exception e) {
-			List<String> list = new ArrayList<>();
-			list.add("メトリクスを計算できませんでした" + e.toString());
-			return list;
+			System.err.println("Failed to calcurate metrics\n" + e);
 		}
 	}
 }
@@ -85,87 +68,23 @@ class WarningContentProvider implements ITreeContentProvider {
 
 	@Override
 	public Object[] getChildren(Object parentElement) {
-		/*
-		 * if (parentElement instanceof Warning) { return ((Warning)
-		 * parentElement).suggestions().toArray(); } return null;
-		 */
-		return ((Node) parentElement).getChildren().toArray();
+		return ((Node<?, ?>) parentElement).getChildren().toArray();
 	}
 
 	@Override
 	public Object getParent(Object element) {
-		// return ((Suggestion) element).parentWarning();
-		return ((Node) element).getParent();
+		return ((Node<?, ?>) element).getParent();
 	}
 
 	@Override
 	public boolean hasChildren(Object element) {
-		/*
-		 * if (element instanceof Warning) { return true; } return false;
-		 */
-		return ((Node) element).hasChildren();
+		return ((Node<?, ?>) element).hasChildren();
 	}
-
 }
 
 class ExplororLabelProvider extends LabelProvider {
-	/*
-	 * public Image getImage(Object element) { if (((File)
-	 * element).isDirectory()) { return
-	 * ViewPlugin.getImageDescriptor("icons/folder.gif").createImage(); } else {
-	 * return ViewPlugin.getImageDescriptor("icons/file.gif").createImage(); } }
-	 */
 	@Override
 	public String getText(Object element) {
-		/*
-		 * if (element instanceof Warning) { return ((Warning)
-		 * element).getMessage(); } else if (element instanceof Suggestion) {
-		 * return ((Suggestion) element).message(); } return element.toString();
-		 */
-		return ((Node) element).getLabel();
+		return ((Node<?, ?>) element).getLabel();
 	}
-}
-
-class DummyWarning extends Warning {
-	private List<Suggestion> suggestions = new ArrayList<>();
-
-	public DummyWarning() {
-		super(null, null, null);
-		suggestions.add(new SplitMethodSuggestion(this));
-	}
-
-	@Override
-	public String getMessage() {
-		return "test";
-	}
-
-	@Override
-	public List<Suggestion> suggestions() {
-		return suggestions;
-	}
-
-	@Override
-	public List<Suggestion> getChildren() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public MetricsCategory getParent() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean hasChildren() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public String getLabel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
