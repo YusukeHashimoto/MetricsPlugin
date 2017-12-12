64e582c7b3fc0bb5b85bd2b5ce97f4dac20f1ea7
Yusuke Hashimoto <yusuke84moto@gmail.com>
2017-12-12 18:32:29 +0900

    CodeAnalyzerの不要なコードを削除

--- src/codeanalyzer/CodeAnalyzer.java
+++ src/codeanalyzer/CodeAnalyzer.java
@@ -23,12 +23,10 @@ public class CodeAnalyzer {
 	private Map<String, ClassInfo> ci = new HashMap<>();
 
 	public static void main(String args[]) {
-		// new CodeAnalyzer().run("src/codeanalizer/");
 		new CodeAnalyzer().run("src/codeanalyzer/");
 	}
 
 	public void run(String... pathsToPackage) {
-
 		for (String path : pathsToPackage) {
 			run(path);
 		}
@@ -45,8 +43,6 @@ public class CodeAnalyzer {
 		}
 		abstractness /= classList.size();
 		Log.info("Abstractness: " + abstractness);
-		// Log.print();
-		// new PackageInfo(pathToPackage, ci);
 	}
 
 	/**
@@ -75,32 +71,18 @@ public class CodeAnalyzer {
 
 		if (visitor.isAbstract())
 			abstractness++;
-		/*
-		 * for(MethodInvocation mi: visitor.getMethodInvocations()) {
-		 * System.out.println("\t" + mi.toString()); }
-		 */
+
 		Log.info("SuperClass: " + visitor.getSuperClass());
 		ci.entrySet().stream().filter(c -> c.getValue().getPackageName() != null)
 				.forEach(c -> Log.info(c.getValue().getPackageName()));
 
-		// printMethodDetail(unit, formattedCode);
-		// printVariableDetail(unit, formattedCode);
-
-		// showWarning(unit, formattedCode, pathToPackage + className);
-		warnings.addAll(warnings(unit, formattedCode, pathToPackage + fileName));
-
-		// ci.add(new ClassInfo(visitor, fileName));
+		//warnings.addAll(warnings(unit, formattedCode, pathToPackage + fileName));
 
 		for (ClassInfo c : visitor.classInfoSet()) {
-			// ClassInfo c = visitor.newClassInfo();
 			ci.put(c.getClassName(), c);
-			// System.out.println(c.getClassName());
-
 			PackageInfo pi = new PackageInfo(pathToPackage, ci);
 			System.out.println();
 		}
-		// c.lackOfCohesionInMethods();
-
 	}
 
 	public void analyzeCodes(ICompilationUnit unit, String pathToPackage, IProject iproject) {
@@ -148,7 +130,6 @@ public class CodeAnalyzer {
 		visitor.getMethodInvocations().stream().forEach(m -> Log.verbose("MethodInvocation: " + m.toString()));
 
 		for (ClassInfo c : visitor.classInfoSet()) {
-			// ClassInfo c = visitor.newClassInfo();
 			ci.put(c.getClassName(), c);
 			Log.info("packages used from " + pathToPackage + filename + " {");
 			c.efficientCouplings(ClassInfo.COUPLING_LEVEL_CLASS).stream().forEach(p -> Log.info("\t" + p));
@@ -156,42 +137,6 @@ public class CodeAnalyzer {
 			Log.info("}");
 		}
 		genClassMetrics(visitor.classInfoSet());
-		Log.print(Log.ERROR);
-		// Log.print();
-	}
-
-	private static void printMethodDetail(CompilationUnit unit, String code) {
-		MyVisitor visitor = new MyVisitor(code);
-		unit.accept(visitor);
-		for (MethodDeclaration method : visitor.getMethodList()) {
-			System.out.printf("蜿ｯ隕匁�ｧ遲�  =%s%n", method.modifiers());
-			System.out.printf("謌ｻ繧雁梛    =%s%n", method.getReturnType2());
-			System.out.printf("繝｡繧ｽ繝�繝牙錐=%s%n", method.getName().getIdentifier());
-			System.out.printf("蠑墓焚      =%s%n", method.parameters());
-			// System.out.printf("譛ｬ菴� =%s%n", method.getBody());
-			System.out.printf("陦梧焚 =%s%n", method.properties().get(MyVisitor.LINE_COUNT));
-			System.out.printf("McCabe=%s%n", method.properties().get(MyVisitor.CYCLOMATIC_COMPLEXITY));
-			System.out.println();
-		}
-	}
-
-	private static void printVariableDetail(CompilationUnit unit, String code) {
-		MyVisitor visitor = new MyVisitor(code);
-		unit.accept(visitor);
-		for (VariableDeclarationFragment variable : visitor.getVariableList()) {
-			System.out.printf("螟画焚蜷�   =%s%n", variable.getName().getIdentifier());
-			System.out.printf("髢句ｧ玖｡� =%s%n", variable.getProperty(MyVisitor.DECLARED_LINE));
-			System.out.printf("蛻晄悄蛹門ｭ�  =%s%n", variable.getInitializer());
-
-			if (variable.getProperty(MyVisitor.DEFINITION_PLACE) instanceof MethodDeclaration) {
-				System.out.println("繝ｭ繝ｼ繧ｫ繝ｫ螟画焚");
-				System.out.printf("蟇ｿ蜻ｽ=%s%n", variable.getProperty(MyVisitor.LIFE_SPAN));
-			} else {
-				System.out.println("繝輔ぅ繝ｼ繝ｫ繝牙､画焚");
-			}
-
-			System.out.println();
-		}
 	}
 
 	private static void setLineNum(MyVisitor formattedVisitor, String rawCode) {
@@ -222,28 +167,6 @@ public class CodeAnalyzer {
 			methList.get(i).setProperty(MyVisitor.DECLARED_LINE,
 					unit.getLineNumber(visitor.getMethodList().get(i).getStartPosition()));
 		}
-	}
-
-	private static List<Warning> warnings(CompilationUnit unit, String code, String filename) {
-		List<Warning> warnings = new ArrayList<>();
-		MyVisitor visitor = new MyVisitor(code);
-		unit.accept(visitor);
-
-		visitor.getVariableList().stream()
-				.filter(v -> (v.getProperty(MyVisitor.DEFINITION_PLACE) instanceof MethodDeclaration)
-						&& lifeSpanOf(v) > THRESHOLD_OF_LIFE_SPAN)
-				.sorted(comparing(CodeAnalyzer::lifeSpanOf).reversed())
-				.forEach(node -> warnings.add(new LargeScopeWarning(unit, node, filename, lifeSpanOf(node))));
-
-		visitor.getMethodList().stream().filter(m -> lineCountOf(m) > THRESHOLD_OF_LINE_COUNT_OF_METHOD)
-				.sorted(comparing(CodeAnalyzer::lineCountOf).reversed())
-				.forEach(node -> warnings.add(new LargeMethodWarning(unit, node, filename, lineCountOf(node))));
-
-		visitor.getMethodList().stream().filter(m -> cyclomaticComplexityOf(m) > THRESHOLD_OF_CYCLOMATIC_CONPLEXITY)
-				.sorted(comparing(CodeAnalyzer::cyclomaticComplexityOf).reversed()).forEach(node -> warnings
-						.add(new ComplexMethodWarning(unit, node, filename, cyclomaticComplexityOf(node))));
-
-		return warnings;
 	}
 
 	private List<Warning> warnings(ClassInfo ci, String filename) {
